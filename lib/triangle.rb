e612fbe168e62b6ee54fafc5817d6aae530d0db2
Edsel Caprice <caprice@mail.usf.edu>
2018-07-12 01:11:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+attr_accessor :equilateral, :isosceles, :scalene
+triangle = {:equilateral, :isosceles, :scalene}
+
+  def initialize(triangle)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+
+
+
 end
