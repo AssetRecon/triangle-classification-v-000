23922a54a3678c5a84f8916ef96086d4b178198b
Edsel Caprice <caprice@mail.usf.edu>
2018-07-12 01:22:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,33 @@
 class Triangle
-attr_accessor :equilateral, :isosceles, :scalene
-triangle = {:equilateral, :isosceles, :scalene}
-
-  def initialize(triangle)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+  attr_accessor :left, :right, :center
+  
+  def initialize(left, right, center)
+    @left = left
+    @right = right
+    @center = center
   end
 
+  def kind
+    if @left == @right == @center
+      :equilateral
+    elsif @left == @right || @right == @center || @left == @center 
+      :isosceles
+    else
+      :scalene
+    end
+
+    if @left == 0 || @right == 0 || @center == 0
+      TriangleError
+    elsif 
+
+
+      
+    if self.
+
+  end
+
+  class TriangleError < StandardError
+  end
 
 
 end
